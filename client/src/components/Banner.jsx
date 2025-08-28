b5cd35952239ca4e2f9bbf5abdde14e1cc77bfe8
rseif963 <rseif0380@gmail.com>
2025-08-28 09:20:53 +0300

    Update Banner.jsx

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
       <div className='text-white'>
         <h2 className='text-3xl font-medium'>Do You Own A Car?</h2>
         <p className='mt-2'>Monetize your vehicle effortlessly by leasing it on TopWings Car Hire Limited.</p>
-        <p className='max-w-130'>Lease your car today and earn upto KES50,000 per Month. We take care of insurance, driver verification and secure payments so you can earn passive income, stress-free.</p>
+        <p className='max-w-130'>Lease your car today and earn upto KES 50,000 per Month. We take care of insurance, driver verification and secure payments so you can earn passive income, stress-free.</p>
         <button className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-pointer'>Lease Your Car</button>
       </div>
       <img src={assets.banner_car_image} alt="car" className='max-h-45 mt-10' />
